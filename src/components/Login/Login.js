aedb1c11e32f7513c82ab7c379475fe4ede599cd
mainuddin-sani <main12sani@gmail.com>
2022-06-07 12:13:17 +0600

    create a login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // email get value
+    const emailHandler = event =>{
+        setEmail(event.target.value);
+    }
+    const passwordHandler = event =>{
+        setPassword(event.target.value);
+    }
+
+    const formSubmitHandler = event => {
+        event.preventDefault();
+    }
     return (
         <div>
             <h1>Login </h1>
-            <form action="#">
+            <form action="#" onSubmit={formSubmitHandler}>
                 <div className="login-heads">
                     <div className="form-group">
-                        <input type="email" placeholder='Enter Your Email' />
+                        <input type="email" onBlur={emailHandler}  placeholder='Enter Your Email' />
                     </div>
                     <div className="form-group">
-                        <input type="password" placeholder='Enter Your password' />
+                        <input type="password" onBlur={passwordHandler} placeholder='Enter Your password' />
                     </div>
-                    <button>Login</button>
+                    <button type='submit'>Login</button>
                 </div>
             </form>
         </div>
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZVzrtpFjDw-xXOD3kg7hHs-aXTe45PPM",
+  authDomain: "fir-auth-8cf7d.firebaseapp.com",
+  projectId: "fir-auth-8cf7d",
+  storageBucket: "fir-auth-8cf7d.appspot.com",
+  messagingSenderId: "358624668610",
+  appId: "1:358624668610:web:613788a86a67ef7e31845b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
