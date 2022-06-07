8fb0dd0ae2b035997967a090bdd72471463c5783
mainuddin-sani <main12sani@gmail.com>
2022-06-07 23:42:40 +0600

    navigate auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import auth from "../../firebase.init";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    // email get value
-    const emailHandler = event =>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [signInWithEmailAndPassword, user, error, loading] =
+    useSignInWithEmailAndPassword(auth);
+  
+      let navigate = useNavigate();
+      // email get value
+      const emailHandler = (event) => {
         setEmail(event.target.value);
-    }
-    const passwordHandler = event =>{
+      };
+      const passwordHandler = (event) => {
         setPassword(event.target.value);
-    }
-
-    const formSubmitHandler = event => {
+      };
+    
+      if(user){
+        navigate('/home');
+      }
+      const formSubmitHandler = (event) => {
         event.preventDefault();
-    }
+        signInWithEmailAndPassword(email, password);
+      };
     return (
         <div>
             <h1>Login </h1>
             <form action="#" onSubmit={formSubmitHandler}>
-                <div className="login-heads">
-                    <div className="form-group">
-                        <input type="email" onBlur={emailHandler}  placeholder='Enter Your Email' />
-                    </div>
-                    <div className="form-group">
-                        <input type="password" onBlur={passwordHandler} placeholder='Enter Your password' />
-                    </div>
-                    <button type='submit'>Login</button>
-                </div>
-            </form>
+        <div className="login-heads">
+          <div className="form-group">
+            <input
+              type="email"
+              onBlur={emailHandler}
+              placeholder="Enter Your Email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              onBlur={passwordHandler}
+              placeholder="Enter Your password"
+            />
+          </div>
+          {
+              loading && <p>Loading...</p>
+          }
+          <p style={{ color: "red" }}>{error?.message}</p>
+          <button type="submit">Login</button>
+        </div>
+      </form>
         </div>
     );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useState } from "react";
+import auth from "../../firebase.init";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            <h1>Register</h1>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [createUserWithEmailAndPassword, user] =
+    useCreateUserWithEmailAndPassword(auth);
+
+    let navigate = useNavigate();
+  // email get value
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+  const confirmPasswordHandler = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  if(user){
+    navigate('/home');
+  }
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(email, password);
+    if (password !== confirmPassword) {
+      setError("Password doesn't match...");
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form action="#" onSubmit={formSubmitHandler}>
+        <div className="login-heads">
+          <div className="form-group">
+            <input
+              type="email"
+              onBlur={emailHandler}
+              placeholder="Enter Your Email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              onBlur={passwordHandler}
+              placeholder="Enter Your password"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              onBlur={confirmPasswordHandler}
+              placeholder="Enter Your Confirm password"
+            />
+          </div>
+          <p style={{ color: "red" }}>{error}</p>
+          <button type="submit">Login</button>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default Register;
